b5830d56155ae86fb77c6253bca1ce5296d03b03
Kyle Strate <kstrate@MacBook-Pro.local>
2022-08-31 11:05:13 -0600

    finally got descriptions to work

--- data.js
+++ data.js
@@ -24,9 +24,8 @@ exports.tasks = [
     {title: "Get Ready to Go Out", description:"Cooperate getting dressed and ready to leave the house", points: 5},
     {title: "Be Kind to Family", description:"All day task: be kind to all family members", points: 20},
     {title: "Good Table Manners", description:"Be on best behaviour for a meal", points: 10},
-    {title: "5 Point Task", description:"Miscellaneous small task", points: 5},
     {title: "10 Point Task", description:"Miscellaneous small task", points: 10},
     {title: "15 Point Task", description:"Miscellaneous medium task", points: 15},
-    {title: "20 Point Task", description:"Miscellaneous big task", points: 20},
+    {title: "20 Point Task", description:"Miscellaneous medium task", points: 20},
     {title: "25 Point Task", description:"Miscellaneous big task", points: 25}
 ]
